535ca2d1d392058d0ce97399c7eff57fcea52bff
tfletcher27 <tfletcher27@governors.school>
2026-04-22 19:34:07 +0000

    WORKKKKKK

--- src/main/java/unit5/Unit5.java
+++ src/main/java/unit5/Unit5.java
@@ -132,7 +132,7 @@ public class Unit5 {
     }
 
     /**
-     * Returns a string representation of this Unit5 object.
+     * Reurns a string representation of this Unit5 object.
      * Format: Unit5{name='[name]', age=[age]}
      *
      * @return formatted string representation
